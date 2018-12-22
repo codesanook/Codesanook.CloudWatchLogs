06ec8b119edfc34d648e7f78954f59fc6aa7589d
aaronamm <theeranitp@gmail.com>
2018-12-22 09:53:37 +0700

    Fix exception in CloudWatch Logs module because alias entity map query requires setting explicit alias

--- ShellEvent.cs
+++ ShellEvent.cs
@@ -7,8 +7,6 @@ using log4net.Repository.Hierarchy;
 using NHibernate.Transform;
 using Orchard.Data;
 using Orchard.Environment;
-using Orchard.Logging;
-using System;
 using System.Collections;
 using System.Linq;
 
@@ -18,32 +16,29 @@ namespace CodeSanook.CloudWatchLogs
     public class ShellEvent : IOrchardShellEvents
     {
         private readonly ITransactionManager transactionManager;
-        public ILogger Logger { get; set; }
 
         public ShellEvent(ITransactionManager transactionManager)
         {
             this.transactionManager = transactionManager;
-            this.Logger = NullLogger.Instance;
         }
 
         public void Activated()
         {
-            try
-            {
-                var hierarchy = ((Hierarchy)LogManager.GetRepository());
-                var rootLogger = hierarchy.Root;
-                var appender = CreateCloudWatchLogAppender();
-                rootLogger.AddAppender(appender);
-
-            }catch(Exception ex)
-            {
-                Logger.Error(ex, ex.Message);
-            }
+            AddCloudWatchLogAppender();
         }
 
         public void Terminating()
         {
         }
+
+        private void AddCloudWatchLogAppender()
+        {
+            var hierarchy = ((Hierarchy)LogManager.GetRepository());
+            var rootLogger = hierarchy.Root;
+            var appender = CreateCloudWatchLogAppender();
+            rootLogger.AddAppender(appender);
+        }
+
 
         private AWSAppender CreateCloudWatchLogAppender()
         {
@@ -54,21 +49,22 @@ namespace CodeSanook.CloudWatchLogs
             patternLayout.ActivateOptions();
 
             var session = transactionManager.GetSession();
-            var setting = session.QueryOver<ModuleSettingPartRecord>()
+
+            ModuleSettingPartRecord moduleSettingAlias = null; ;
+            var setting = session.QueryOver(() => moduleSettingAlias)
                 .SelectList(list => list
-                    .Select(p => p.AwsAccessKey)
-                    .Select(p => p.AwsSecretKey)
+                    .Select(() => moduleSettingAlias.AwsAccessKey).WithAlias(() => moduleSettingAlias.AwsAccessKey)
+                    .Select(() => moduleSettingAlias.AwsSecretKey).WithAlias(() => moduleSettingAlias.AwsSecretKey)
                 )
                 .TransformUsing(Transformers.AliasToEntityMap)
                 .List<IDictionary>()
                 .FirstOrDefault();
-                   
 
             var appender = new AWSAppender
             {
                 Layout = patternLayout,
                 Credentials = new BasicAWSCredentials(
-                    setting[nameof(ModuleSettingPartRecord.AwsAccessKey)].ToString(), 
+                    setting[nameof(ModuleSettingPartRecord.AwsAccessKey)].ToString(),
                     setting[nameof(ModuleSettingPartRecord.AwsSecretKey)].ToString()
                 ),
                 LogGroup = "CodeSanook.CloudWatchLog",
@@ -78,6 +74,5 @@ namespace CodeSanook.CloudWatchLogs
             appender.ActivateOptions();
             return appender;
         }
-
     }
 }
